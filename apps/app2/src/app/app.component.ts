4a5a33ae31d594129adcd6859d79f2fb04a77427
crowelm <chris.rowe@landmark.co.uk>
2021-08-25 09:10:56 +0100

    Added app2

---
+++ apps/app2/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'testorg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'app2';
+}
